51c015c745fd2380a4997a57d621aa9b68ebc03b
David Waite <dwaite@pingidentity.com>
2020-07-29 02:24:51 -0600

    System keeps adding a second target

--- .targets.mk
+++ .targets.mk
@@ -1,4 +1,4 @@
-TARGETS_DRAFTS := draft-waite-multipass-retrieval draft-waite-restricted-jsonld
+TARGETS_DRAFTS := draft-waite-multipass-retrieval
 TARGETS_TAGS := 
 draft-waite-multipass-retrieval-00.md: draft-waite-multipass-retrieval.md
 	sed -e 's/draft-waite-multipass-retrieval-latest/draft-waite-multipass-retrieval-00/g' -e 's/draft-waite-restricted-jsonld-latest/draft-waite-restricted-jsonld-00/g' $< >$@
